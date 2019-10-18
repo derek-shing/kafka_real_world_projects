269bbf477efd7f67fc4deee5f5b551cae20b9578
derek-shing <hpshing@gmail.com>
2019-10-17 23:59:37 -0700

    Complete producer basic

--- src/main/java/kafka/tutorial/ConsumerDemo.java
+++ src/main/java/kafka/tutorial/ConsumerDemo.java
@@ -1,8 +1,13 @@
 package kafka.tutorial;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class ConsumerDemo {
@@ -10,16 +15,36 @@ public class ConsumerDemo {
         //Set Consumer properties
 
         String server = "127.0.0.1:9092";
+        String groupId = "my-fourth-application";
+        String topic = "first_topic";
 
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty((ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName()));
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
 
 
         //Start Consumer
 
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
+
+
         //Subscribe topic
+
+        consumer.subscribe(Arrays.asList(topic));
+
+
+        //Pool for Data
+
+        while (true){
+            ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(300));
+
+            for (ConsumerRecord<String,String> record: records){
+                System.out.println(record.value());
+            }
+        }
 
     }
 }
